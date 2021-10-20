40cb202a8e96ee968f7085bc992bc750113798e4
Gamy <flokygamy@gmail.com>
2021-10-20 13:03:21 +0000

    added edit and delete user actions

--- src/actions/userActions.jsx
+++ src/actions/userActions.jsx
@@ -1,8 +1,18 @@
-const addUser = (newUser) => {
+export const addUser = (newUser) => {
     return {
         type: 'ADD_USER',
         payload: newUser,
     };
 };
-
-export default addUser;
+export const editUser = (userId, updatedUser) => {
+    return {
+        type: "EDIT_USER",
+        payload: { userId, updatedUser }
+    };
+};
+export const deleteUser = (userId) => {
+    return {
+        type: "DELETE_USER",
+        payload: userId
+    };
+};
