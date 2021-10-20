285175bf1ad686fe70a755f22387c0ba0b36987e
Gamy <flokygamy@gmail.com>
2021-10-20 13:08:31 +0000

    implemented edit and delete users

--- src/reducers/usersReducers.js
+++ src/reducers/usersReducers.js
@@ -5,7 +5,20 @@ const initialState = {
 const usersReducers = (state = initialState, action) => {
     switch (action.type) {
         case 'ADD_USER':
-            return { ...state, users: [...state.users, action.payload] }
+            return { ...state, users: [...state.users, action.payload] };
+        case 'EDIT_USER':
+            const updatedUsers = state.users.map((user) => {
+                if (user.id === action.payload.userId) {
+                    return action.payload.updatedUser
+                }
+                return user;
+            });
+            return { ...state, users: updatedUsers };
+        case 'DELETE_USER':
+            const deletedUser = state.users.filter((user) => {
+                return user.id !== action.payload
+            })
+            return {...state, users: deletedUser}
         default:
             return state
     }
