94289c84ca898b70a7234b6762190ba08acbc611
Gamy <flokygamy@gmail.com>
2021-10-27 12:41:36 +0000

    add firebase config

---
+++ src/firebase/Config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyADMUSUdnXCyPnnOVNsIVr0rRzyj67OERw",
+    authDomain: "gaamy-7096c.firebaseapp.com",
+    projectId: "gaamy-7096c",
+    storageBucket: "gaamy-7096c.appspot.com",
+    messagingSenderId: "489776321538",
+    appId: "1:489776321538:web:3e0a90783c74eacae3431b"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  
+export default firebase
