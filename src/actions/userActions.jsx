9481b118a310e98165ba56f1e5f5a025abc943f0
Gamy <flokygamy@gmail.com>
2021-11-09 01:55:20 +0000

    implamented addUser with firestore cloud

--- src/actions/userActions.jsx
+++ src/actions/userActions.jsx
@@ -1,8 +1,18 @@
 export const addUser = (newUser) => {
-    return {
-        type: 'ADD_USER',
-        payload: newUser,
+    return (dispatch, state, { getFirestore }) => {
+        getFirestore().collection("users").add(newUser)
+            .then(() => {
+                dispatch({
+                    type: 'ADD_USER',
+                    payload: newUser
+                });
+            });
+
     };
+    // return {
+    //     type: 'ADD_USER',
+    //     payload: newUser,
+    // };
 };
 export const editUser = (userId, updatedUser) => {
     return {
@@ -16,3 +26,17 @@ export const deleteUser = (userId) => {
         payload: userId
     };
 };
+
+export const getAllUsers = () => {
+    return (dispatch, state, { getFirestore }) => {
+        getFirestore().collection('users')
+            .onSnapshot((query) => {
+                const Users = [];
+                query.forEach((doc) => {
+                    Users.push(doc.data());
+                });
+                console.log(Users);
+                dispatch({ type: "ALL_USERS", payload: Users });
+          })
+    }
+}
