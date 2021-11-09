2f3bb7461def463b6ac40ca557fa55218f15cc6f
Gamy <flokygamy@gmail.com>
2021-11-09 01:54:26 +0000

    imported connect and getAllUsers actions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react"
+import React, { useEffect } from "react"
 import './App.css';
 import UserList from "./components/UserList";
 import UsersForm from "./components/UsersForm";
+import { connect } from "react-redux";
+import { getAllUsers } from "./actions/userActions.jsx";
 
-function App() {
- 
+function App(props) {
+
+  useEffect(() => {
+    props.getAllUsers();
+ },[props])
   return (
     <div className="main">
       <h1 className="text-center mb-3"> displaying userList in React</h1>
@@ -13,5 +18,8 @@ function App() {
     </div>
   );
 }
+const mapDispatchToProps = {
+  getAllUsers ,
+}
 
-export default App;
+export default connect(null,mapDispatchToProps) (App);
