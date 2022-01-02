6735e7e8adca00bec39381b0de5c373d7e47f3a0
Gamy <flokygamy@gmail.com>
2022-01-02 06:25:14 +0000

    changed project to anotherone

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -3,13 +3,14 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyADMUSUdnXCyPnnOVNsIVr0rRzyj67OERw",
-    authDomain: "gaamy-7096c.firebaseapp.com",
-    projectId: "gaamy-7096c",
-    storageBucket: "gaamy-7096c.appspot.com",
-    messagingSenderId: "489776321538",
-    appId: "1:489776321538:web:3e0a90783c74eacae3431b"
-  };
+  apiKey: "AIzaSyDJN5oQd3myNkA3FM-HUkvsYLNRXEmnGpo",
+  authDomain: "anotherone-56228.firebaseapp.com",
+  projectId: "anotherone-56228",
+  storageBucket: "anotherone-56228.appspot.com",
+  messagingSenderId: "79784839013",
+  appId: "1:79784839013:web:5843cd4ac5d577e507038c",
+  measurementId: "G-T586TBQHVZ"
+};
   
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
